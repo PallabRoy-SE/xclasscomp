9df0b40187e57067270c53040133520ed2030dd0
PALLAB ROY <76547199+PallabRoy-SE@users.noreply.github.com>
2024-05-11 00:39:25 +0530

    refactor: change to class component

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,24 +1,29 @@
-import React, { useState } from "react";
+import { Component } from 'react';
 
-function CounterApp() {
-  const [count, setCount] = useState(0);
+class CounterApp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0,
+        };
+    }
+    increment = () => {
+        this.setState({ ...this.state, count: this.state.count + 1 });
+    };
+    decrement = () => {
+        this.setState({ ...this.state, count: this.state.count - 1 });
+    };
 
-  const increment = () => {
-    setCount(count + 1);
-  };
-
-  const decrement = () => {
-    setCount(count - 1);
-  };
-
-  return (
-    <div>
-      <h1>Counter App</h1>
-      <p>Count: {count}</p>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-    </div>
-  );
+    render() {
+        return (
+            <div>
+                <h1>Counter App</h1>
+                <p>Count: {this.state.count}</p>
+                <button onClick={() => this.increment()}>Increment</button>
+                <button onClick={() => this.decrement()}>Decrement</button>
+            </div>
+        );
+    }
 }
 
 export default CounterApp;
